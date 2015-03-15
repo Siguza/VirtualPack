2168801d9437219876f6220cdd9166e61ce35642
Siguza <siguza@siguza.net>
2015-03-15 20:19:21 +0100

    Fixed Anvil

--- src/main/java/net/drgnome/virtualpack/components/VAnvil.java
+++ src/main/java/net/drgnome/virtualpack/components/VAnvil.java
@@ -6,6 +6,7 @@ package net.drgnome.virtualpack.components;
 
 import java.util.*;
 import java.lang.reflect.*;
+import org.apache.commons.lang3.StringUtils;
 import net.minecraft.server.v#MC_VERSION#.*;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -23,7 +24,8 @@ public class VAnvil extends ContainerAnvil
         super(entityplayer.inventory, null, new BlockPosition(0, 0, 0), entityplayer);
         checkReachable = false;
         _player = entityplayer;
-        for(int i = 0; i < #FIELD_CONTAINER_3#.size(); i++)
+        #FIELD_CONTAINER_3#.set(2, new VAnvilSlot(this, getInv("#FIELD_CONTAINERANVIL_2#"), 2, 134, 47));
+        /*for(int i = 0; i < #FIELD_CONTAINER_3#.size(); i++)
         {
             try
             {
@@ -38,7 +40,12 @@ public class VAnvil extends ContainerAnvil
                 e.printStackTrace();
                 break;
             }
-        }
+            if(#FIELD_CONTAINER_3#.get(i) instanceof SlotResult)
+            {
+                #FIELD_CONTAINER_3#.set(i, new VAnvilSlot(this, getInv("#FIELD_CONTAINERANVIL_2#"), 2, 134, 47));
+                break;
+            }
+        }*/
     }
 
     public VAnvil(EntityPlayer entityplayer, int x, int y, int z)
@@ -56,17 +63,211 @@ public class VAnvil extends ContainerAnvil
         }
     }
 
-    public void #FIELD_CONTAINERANVIL_4#()
+    public final ItemStack clickItem(int slot, int mouse, int shift, EntityHuman human)
     {
-        calculate0();
+        ItemStack stack = super.clickItem(slot, mouse, shift, human);
         updatePlayerInventory();
+        return stack;
     }
 
-    public void calculate0()
+    public void #FIELD_CONTAINERANVIL_4#()
+    {
+        final int maxLevel = Config.getInt(_player.getBukkitEntity(), "tools", "anvil", "maxlevel", Config.MODE_MAX);
+        IInventory invG = getInv("#FIELD_CONTAINERANVIL_2#");
+        IInventory invH = VAnvil.#FIELD_CONTAINERANVIL_1#(this);
+        boolean flag = false;
+        boolean flag1 = true;
+        boolean flag2 = true;
+        boolean flag3 = true;
+        boolean flag4 = true;
+        boolean flag5 = true;
+        boolean flag6 = true;
+        ItemStack itemstack = invH.getItem(0);
+        #FIELD_CONTAINERANVIL_5# = 1;
+        int i = 0;
+        byte b0 = 0;
+        byte b1 = 0;
+        if(itemstack == null)
+        {
+            invG.setItem(0, (ItemStack) null);
+            #FIELD_CONTAINERANVIL_5# = 0;
+        }
+        else
+        {
+            ItemStack itemstack1 = itemstack.cloneItemStack();
+            ItemStack itemstack2 = invH.getItem(1);
+            Map map = EnchantmentManager.#FIELD_ENCHANTMENTMANAGER_3#(itemstack1);
+            boolean flag7 = false;
+            int j = b0 + itemstack.getRepairCost() + (itemstack2 == null ? 0 : itemstack2.getRepairCost());
+            _exp = 0;
+            int k;
+            if(itemstack2 != null)
+            {
+                flag7 = itemstack2.getItem() == Items.ENCHANTED_BOOK && Items.ENCHANTED_BOOK.#FIELD_ITEMENCHANTEDBOOK_2#(itemstack2).size() > 0;
+                int l;
+                int i1;
+                if(itemstack1.#FIELD_ITEMSTACK_10#() && itemstack1.getItem().#FIELD_ITEM_6#(itemstack, itemstack2))
+                {
+                    k = Math.min(itemstack1.#FIELD_ITEMSTACK_5#(), itemstack1.#FIELD_ITEMSTACK_6#() / 4);
+                    if(k <= 0)
+                    {
+                        invG.setItem(0, (ItemStack)null);
+                        #FIELD_CONTAINERANVIL_5# = 0;
+                        return;
+                    }
+                    for(l = 0; k > 0 && l < itemstack2.count; ++l)
+                    {
+                        i1 = itemstack1.h() - k;
+                        itemstack1.setData(i1);
+                        ++i;
+                        k = Math.min(itemstack1.#FIELD_ITEMSTACK_5#(), itemstack1.#FIELD_ITEMSTACK_6#() / 4);
+                    }
+                    _exp = l;
+                }
+                else
+                {
+                    if(!flag7 && (itemstack1.getItem() != itemstack2.getItem() || !itemstack1.#FIELD_ITEMSTACK_10#()))
+                    {
+                        invG.setItem(0, (ItemStack)null);
+                        #FIELD_CONTAINERANVIL_5# = 0;
+                        return;
+                    }
+                    int j1;
+                    if(itemstack1.#FIELD_ITEMSTACK_10#() && !flag7)
+                    {
+                        k = itemstack.#FIELD_ITEMSTACK_6#() - itemstack.#FIELD_ITEMSTACK_5#();
+                        l = itemstack2.#FIELD_ITEMSTACK_6#() - itemstack2.#FIELD_ITEMSTACK_5#();
+                        i1 = l + itemstack1.#FIELD_ITEMSTACK_6#() * 12 / 100;
+                        int k1 = k + i1;
+                        j1 = itemstack1.#FIELD_ITEMSTACK_6#() - k1;
+                        if(j1 < 0)
+                        {
+                            j1 = 0;
+                        }
+                        if(j1 < itemstack1.getData())
+                        {
+                            itemstack1.setData(j1);
+                            i += 2;
+                        }
+                    }
+                    Map map1 = EnchantmentManager.#FIELD_ENCHANTMENTMANAGER_3#(itemstack2);
+                    Iterator iterator = map1.keySet().iterator();
+                    while(iterator.hasNext())
+                    {
+                        i1 = ((Integer)iterator.next()).intValue();
+                        Enchantment enchantment = Enchantment.getById(i1);
+                        if(enchantment != null)
+                        {
+                            j1 = map.containsKey(Integer.valueOf(i1)) ? ((Integer)map.get(Integer.valueOf(i1))).intValue() : 0;
+                            int l1 = ((Integer)map1.get(Integer.valueOf(i1))).intValue();
+                            int i2;
+                            if(j1 == l1)
+                            {
+                                ++l1;
+                                i2 = l1;
+                            }
+                            else
+                            {
+                                i2 = Math.max(l1, j1);
+                            }
+                            l1 = i2;
+                            boolean flag8 = enchantment.canEnchant(itemstack);
+                            if(playerFree(_player) || itemstack.getItem() == Items.ENCHANTED_BOOK)
+                            {
+                                flag8 = true;
+                            }
+                            Iterator iterator1 = map.keySet().iterator();
+                            while(iterator1.hasNext())
+                            {
+                                int j2 = ((Integer)iterator1.next()).intValue();
+                                if(j2 != i1 && !enchantment.#FIELD_ENCHANTMENT_1#(Enchantment.getById(j2)))
+                                {
+                                    flag8 = false;
+                                    ++i;
+                                }
+                            }
+                            if(flag8)
+                            {
+                                if(l1 > enchantment.getMaxLevel())
+                                {
+                                    l1 = enchantment.getMaxLevel();
+                                }
+                                map.put(Integer.valueOf(i1), Integer.valueOf(l1));
+                                int k2 = 0;
+                                switch(enchantment.getRandomWeight())
+                                {
+                                    case 1:
+                                        k2 = 8;
+                                        break;
+                                    case 2:
+                                        k2 = 4;
+                                        break;
+                                    case 5:
+                                        k2 = 2;
+                                        break;
+                                    case 10:
+                                        k2 = 1;
+                                        break;
+                                }
+                                if(flag7)
+                                {
+                                    k2 = Math.max(1, k2 / 2);
+                                }
+                                i += k2 * l1;
+                            }
+                        }
+                    }
+                }
+            }
+            if(StringUtils.isBlank(_itemName))
+            {
+                if(itemstack.hasName())
+                {
+                    b1 = 1;
+                    i += b1;
+                    itemstack1.#FIELD_ITEMSTACK_11#();
+                }
+            }
+            else if(!_itemName.equals(itemstack.getName()))
+            {
+                b1 = 1;
+                i += b1;
+                itemstack1.#FIELD_ITEMSTACK_9#(_itemName);
+            }
+            #FIELD_CONTAINERANVIL_5# = j + i;
+            if(i <= 0)
+            {
+                itemstack1 = null;
+            }
+            if(b1 == i && b1 > 0 && #FIELD_CONTAINERANVIL_5# >= maxLevel)
+            {
+                #FIELD_CONTAINERANVIL_5# = maxLevel - 1;
+            }
+            if(#FIELD_CONTAINERANVIL_5# >= maxLevel && !playerFree(_player))
+            {
+                itemstack1 = null;
+            }
+            if(itemstack1 != null)
+            {
+                k = itemstack1.getRepairCost();
+                if(itemstack2 != null && k < itemstack2.getRepairCost())
+                {
+                    k = itemstack2.getRepairCost();
+                }
+                k = k * 2 + 1;
+                itemstack1.setRepairCost(k);
+                EnchantmentManager.#FIELD_ENCHANTMENTMANAGER_4#(map, itemstack1);
+            }
+            invG.setItem(0, itemstack1);
+            this.#FIELD_CONTAINER_9#();
+        }
+    }
+
+    public void not_#FIELD_CONTAINERANVIL_4#()
     {
         final int maxLevel = Config.getInt(_player.getBukkitEntity(), "tools", "anvil", "maxlevel", Config.MODE_MAX);
         ItemStack itemstack = VAnvil.#FIELD_CONTAINERANVIL_1#(this).getItem(0);
-        #FIELD_CONTAINERANVIL_5# = 0;
+        #FIELD_CONTAINERANVIL_5# = 1;
         int i = 0;
         byte b0 = 0;
         int j = 0;
@@ -92,7 +293,8 @@ public class VAnvil extends ContainerAnvil
             Enchantment enchantment;
             if(itemstack2 != null)
             {
-                flag = Item.#FIELD_ITEM_7#(itemstack2.getItem()) == Material.ENCHANTED_BOOK.getId() && ((ItemEnchantedBook)Item.#FIELD_ITEM_8#(Material.ENCHANTED_BOOK.getId())).#FIELD_ITEMENCHANTEDBOOK_2#(itemstack2).size() > 0;
+                //flag = Item.#FIELD_ITEM_7#(itemstack2.getItem()) == Material.ENCHANTED_BOOK.getId() && ((ItemEnchantedBook)Item.#FIELD_ITEM_8#(Material.ENCHANTED_BOOK.getId())).#FIELD_ITEMENCHANTEDBOOK_2#(itemstack2).size() > 0;
+                flag = itemstack2.getItem() == Items.ENCHANTED_BOOK && Items.ENCHANTED_BOOK.#FIELD_ITEMENCHANTEDBOOK_2#(itemstack2).size() > 0;
                 if(itemstack1.#FIELD_ITEMSTACK_10#() && itemstack1.getItem().#FIELD_ITEM_6#(itemstack, itemstack2))
                 {
                     l = Math.min(itemstack1.#FIELD_ITEMSTACK_5#(), itemstack1.#FIELD_ITEMSTACK_6#() / 4);
@@ -106,7 +308,8 @@ public class VAnvil extends ContainerAnvil
                     {
                         j1 = itemstack1.#FIELD_ITEMSTACK_5#() - l;
                         itemstack1.setData(j1);
-                        i += Math.max(1, l / 100) + map.size();
+                        //i += Math.max(1, l / 100) + map.size();
+                        i++;
                         l = Math.min(itemstack1.#FIELD_ITEMSTACK_5#(), itemstack1.#FIELD_ITEMSTACK_6#() / 4);
                     }
                     _exp = i1;
@@ -133,7 +336,8 @@ public class VAnvil extends ContainerAnvil
                         if(k1 < itemstack1.getData())
                         {
                             itemstack1.setData(k1);
-                            i += Math.max(1, j1 / 100);
+                            //i += Math.max(1, j1 / 100);
+                            i += 2;
                         }
                     }
                     Map map1 = EnchantmentManager.#FIELD_ENCHANTMENTMANAGER_3#(itemstack2);
@@ -142,37 +346,42 @@ public class VAnvil extends ContainerAnvil
                     {
                         j1 = ((Integer)iterator.next()).intValue();
                         enchantment = Enchantment.getById(j1);
-                        k1 = map.containsKey(Integer.valueOf(j1)) ? ((Integer)map.get(Integer.valueOf(j1))).intValue() : 0;
-                        l1 = ((Integer)map1.get(Integer.valueOf(j1))).intValue();
-                        int j2;
-                        if(k1 == l1)
+                        if(enchantment != null)
                         {
-                            ++l1;
-                            j2 = l1;
-                        }
-                        else
-                        {
-                            j2 = Math.max(l1, k1);
-                        }
-                        l1 = j2;
-                        int k2 = l1 - k1;
-                        boolean flag1 = enchantment.canEnchant(itemstack);
-                        if(playerFree(_player) || (Item.#FIELD_ITEM_7#(itemstack.getItem()) == Material.ENCHANTED_BOOK.getId()))
-                        {
-                            flag1 = true;
-                        }
-                        Iterator iterator1 = map.keySet().iterator();
-                        while(iterator1.hasNext())
-                        {
-                            int l2 = ((Integer)iterator1.next()).intValue();
-                            if(l2 != j1 && !enchantment.#FIELD_ENCHANTMENT_1#(Enchantment.getById(l2)))
+                            k1 = map.containsKey(Integer.valueOf(j1)) ? ((Integer)map.get(Integer.valueOf(j1))).intValue() : 0;
+                            l1 = ((Integer)map1.get(Integer.valueOf(j1))).intValue();
+                            int j2;
+                            if(k1 == l1)
                             {
-                                flag1 = false;
-                                i += k2;
+                                ++l1;
+                                j2 = l1;
                             }
-                        }
-                        if(flag1)
-                        {
+                            else
+                            {
+                                j2 = Math.max(l1, k1);
+                            }
+                            l1 = j2;
+                            int k2 = l1 - k1;
+                            boolean flag1 = enchantment.canEnchant(itemstack);
+                            if(playerFree(_player) || (Item.#FIELD_ITEM_7#(itemstack.getItem()) == Material.ENCHANTED_BOOK.getId()))
+                            {
+                                flag1 = true;
+                            }
+                            Iterator iterator1 = map.keySet().iterator();
+                            while(iterator1.hasNext())
+                            {
+                                int l2 = ((Integer)iterator1.next()).intValue();
+                                if(l2 != j1 && !enchantment.#FIELD_ENCHANTMENT_1#(Enchantment.getById(l2)))
+                                {
+                                    flag1 = false;
+                                    //i += k2;
+                                    i++;
+                                }
+                            }
+                            if(flag1)
+                            {
+                                continue;
+                            }
                             if(l1 > enchantment.getMaxLevel())
                             {
                                 l1 = enchantment.getMaxLevel();
@@ -186,26 +395,39 @@ public class VAnvil extends ContainerAnvil
                                     break;
                                 case 2:
                                     i3 = 4;
-                                case 3:
-                                case 4:
-                                case 6:
-                                case 7:
-                                case 8:
-                                case 9:
-                                default:
                                     break;
                                 case 5:
                                     i3 = 2;
                                     break;
                                 case 10:
                                     i3 = 1;
+                                    break;
+                            }
+                            if(flag)
+                            {
+                                i3 = Math.max(1, i3 / 2);
                             }
                             i += i3 * k2;
                         }
                     }
                 }
             }
-            if(_itemName != null && !_itemName.equalsIgnoreCase(itemstack.getName()) && _itemName.length() > 0)
+            if(_itemName == null || _itemName.length() < 1)
+            {
+                if(itemstack.hasName())
+                {
+                    j = 1;
+                    i += j;
+                    itemstack1.#FIELD_ITEMSTACK_11#();
+                }
+            }
+            else if(!_itemName.equalsIgnoreCase(itemstack.getName()))
+            {
+                j = 1;
+                i += j;
+                itemstack1.#FIELD_ITEMSTACK_9#(_itemName);
+            }
+            /*if(_itemName != null && !_itemName.equalsIgnoreCase(itemstack.getName()) && _itemName.length() > 0)
             {
                 j = itemstack.#FIELD_ITEMSTACK_10#() ? 7 : itemstack.count * 5;
                 i += j;
@@ -214,8 +436,9 @@ public class VAnvil extends ContainerAnvil
                     k += j / 2;
                 }
                 itemstack1.#FIELD_ITEMSTACK_9#(_itemName);
-            }
-            l = 0;
+            }*/
+
+            /*l = 0;
             for(iterator = map.keySet().iterator(); iterator.hasNext(); k += l + k1 * l1)
             {
                 j1 = ((Integer)iterator.next()).intValue();
@@ -252,7 +475,7 @@ public class VAnvil extends ContainerAnvil
             if(flag)
             {
                 k = Math.max(1, k / 2);
-            }
+            }*/
             #FIELD_CONTAINERANVIL_5# = k + i;
             if(i <= 0)
             {
@@ -297,7 +520,7 @@ public class VAnvil extends ContainerAnvil
             ItemStack itemstack = VAnvil.#FIELD_CONTAINERANVIL_1#(this).splitWithoutUpdate(i);
             if(itemstack != null)
             {
-                entityhuman.drop(itemstack, false); // What does this "false" do?
+                entityhuman.drop(itemstack, false); // What does this "false" do? - Who am I even asking?
             }
         }
     }
